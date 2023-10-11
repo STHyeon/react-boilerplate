5fc383db6518db143233231ebe56c93e8c9039cf
taehyeon <dulgi@kakao.com>
2023-10-11 14:12:27 +0900

    chore(webpack): update config

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,17 @@
-module.exports = {
-  presets: [
-    "@babel/preset-react",
-    "@babel/preset-env",
-    "@babel/preset-typescript",
-  ],
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    '@babel/preset-react',
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+  ];
+
+  //
+  //
+  //
+  
+  return {
+    presets,
+  };
 };
